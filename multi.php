fbf4af43cc55084d5cd6aae12320e576be3909fc
fivegins <fivegins@gmail.com>
2018-12-25 20:11:32 +0700

    Add files via upload

---
+++ multi.php
@@ -0,0 +1,24 @@
+<?php
+
+include 'functions.php';
+
+$link = $_GET['link'];
+$s = $_GET['s'];
+$e = $_GET['e'];
+$single_curl = single_curl($link);
+
+$links = array();
+if ($s == 1) {
+	$links[] = $link;
+}
+for ($i = $s; $i <= $e; $i++) { 
+	$links[] = "{$link}{$i}/";
+}
+
+$multi_curl = multi_curl($links);
+
+preg_match_all('#<div style="background:\#f7f7f7.*?">(.*?)</div>\s*<center><div class="phantrang">#is', $multi_curl, $list_chapter);
+
+foreach ($list_chapter[1] as $value) {
+	echo strip_tags(loc($value), '<p>, <strong>, <br>') . '<hr/>';
+}
