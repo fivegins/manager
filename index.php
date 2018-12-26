fa05346ea4995bb3b6c2a798934c46d2850fb05f
fivegins <fivegins@gmail.com>
2018-12-26 12:16:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+<form action="multi.php" method="get">
+	<input type="text" name="link" placeholder="link"><br>
+	<input type="number" name="s" placeholder="s"><br>
+	<input type="number" name="e" placeholder="e"><br>
+	<input type="submit" value="GET">
+</form>
 <?php phpinfo(); ?>
