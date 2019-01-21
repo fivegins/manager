3b86520a41dd4068daebfaa1b096bb537c9f167d
fivegins <fivegins@gmail.com>
2019-01-21 20:43:10 +0700

    Update 1.php

--- 1.php
+++ 1.php
@@ -7,11 +7,13 @@ if (isset($_POST['submit'])) {
 	file_put_contents( "info.txt", json_encode($info) );
 }
 
+$infos = json_decode(file_get_contents("info.txt"), true);
+
 ?>
 <form method="post">
-	<input type="text" name="link" placeholder="link" value="<?php echo $_POST['link'] ?>"><br>
-	<input type="text" name="s" placeholder="s" value="<?php echo $_POST['s'] ?>"><br>
-	<input type="text" name="e" placeholder="e" value="<?php echo $_POST['e'] ?>"><br>
+	<input type="text" name="link" placeholder="link" value="<?php echo (isset($_POST['link'])) ? $_POST['link'] : $infos['link'] ?>"><br>
+	<input type="text" name="s" placeholder="s" value="<?php echo (isset($_POST['s'])) ? $_POST['s'] : $infos['s'] ?>"><br>
+	<input type="text" name="e" placeholder="e" value="<?php echo (isset($_POST['e'])) ? $_POST['e'] : $infos['e'] ?>"><br>
 	<input type="submit" name="submit" value="GET">
 </form>
 <p><?php echo file_get_contents("data.txt"); ?></p>
